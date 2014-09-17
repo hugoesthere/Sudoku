b5ba8ad2e8f6212e5bf92656fbfbc522f84cc971
Hugo <hbh02012@pomona.edu>
2014-09-17 15:45:11 -0700

    Added some comments

--- Sudoku/HHSLViewController.m
+++ Sudoku/HHSLViewController.m
@@ -21,18 +21,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
-    //create grid frame
+    // Create grid frame
     CGRect frame = self.view.frame;
-    
     CGFloat x = CGRectGetWidth(frame)*.1;
     CGFloat y = CGRectGetHeight(frame)*.1;
     CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame))*.80;
     CGRect gridFrame = CGRectMake(x, y, size, size);
     
+    // Instantiate _gridView and add it to the ViewController
     _gridView = [[HHSLGridView alloc] initWithFrame:gridFrame];
-    
     [self.view addSubview:_gridView];
     
 }
